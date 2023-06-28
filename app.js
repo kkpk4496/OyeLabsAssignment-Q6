686ea0d2ae5ec119b57d08684a7840be129b9031
kkpk4496 <pavankalyank4496@gmail.com>
2023-06-28 07:56:30 +0000

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+function compare(arrays){
+for(let i=0;i<101;i++){
+    if (arrays.find(i)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+}
+arrays=[for(let i=0;i<100;i++)]
+
+sort(arrays)
+
+compare(arrays)
+
+export module=app;
